2c846d00e1f707731a79ab113a2b918b93e07904
azra <azrayazidalkautsar@gmail.com>
2024-01-29 23:21:52 +0700

    update image

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   const [result, setResult] = useState("");
   const [remainingAttempts, setRemainingAttempts] = useState(6);
-  const [lifeImg, setLifeImg] = useState("");
+  const [lifeImg, setLifeImg] = useState("./lifedefault.jpg");
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * words1.length);
@@ -28,33 +28,41 @@ export default function Home() {
   const restartGame = () => {
     window.location.reload();
   };
-
-  const imgLife5 = "./logo192.png";
-  const imgLife4 = "./line1.png";
-  const imgLife3 = "./line1.png";
-  const imgLife2 = "./line1.png";
-  const imgLife1 = "./line1.png";
+  const lifedefault = "./lifedefault.jpg";
+  const imgLife6 = "./life6.jpg";
+  const imgLife5 = "./life5.jpg";
+  const imgLife4 = "./life4.jpg";
+  const imgLife3 = "./life3.jpg";
+  const imgLife2 = "./life2.jpg";
+  const imgLife1 = "./life1.jpg";
 
   useEffect(() => {
     const wordArray = wordToGuess.split("");
     const displayWord = wordArray
       .map((letter) => (guessedLetters.includes(letter) ? letter : "_"))
       .join("");
+
+    console.log("wordArray", wordArray);
+    console.log("displayWord", displayWord);
     if (displayWord === wordToGuess) {
       setResult("You Win!");
     } else if (remainingAttempts === 5) {
       setLifeImg(imgLife1);
-      // setLifeImg(imgLife2);
     } else if (remainingAttempts === 4) {
       setLifeImg("");
       setLifeImg(imgLife2);
     } else if (remainingAttempts === 3) {
-      // setLifeImg(imgLife3);
+      setLifeImg("");
+      setLifeImg(imgLife3);
     } else if (remainingAttempts === 2) {
-      // setLifeImg(imgLife4);
+      setLifeImg("");
+      setLifeImg(imgLife4);
     } else if (remainingAttempts === 1) {
-      // setLifeImg(imgLife4);
+      setLifeImg("");
+      setLifeImg(imgLife5);
     } else if (remainingAttempts === 0) {
+      setLifeImg("");
+      setLifeImg(imgLife6);
       setResult("Game Over!");
       setTimeout(restartGame, 3000);
     }
@@ -66,7 +74,13 @@ export default function Home() {
       <div className="flex flex-col items-center">
         <h1 className="text-3xl font-bold my-4">Hangman Game</h1>
         <GuessWords wordToGuess={wordToGuess} guessedLetters={guessedLetters} />
-        <div>Attempts Remaining: {remainingAttempts}</div>
+        <div
+          className={`text-gray-800 ${
+            remainingAttempts === 0 ? "text-white" : ""
+          }`}
+        >
+          Attempts Remaining: {remainingAttempts}
+        </div>
         <div>{result}</div>
       </div>
       <KeyboardText
@@ -75,11 +89,6 @@ export default function Home() {
         wordToGuess={wordToGuess}
       />
       <img src={`${lifeImg}`} />
-      {/* <img
-        src="./421248785_914166843659250_5482120767074255287_n.jpg"
-        alt="Description of the image"
-      /> */}
-      <img src="" />
     </div>
   );
 }
